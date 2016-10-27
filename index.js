48c2b25f5ae4f04e2ec36c3b163817ec12312fe2
Yura <yyinprogramming@gmail.com>
2016-10-27 20:16:51 +0300

    one more RegEx

--- index.js
+++ index.js
@@ -82,13 +82,14 @@ function createEvent(collection, date, sender) {
 	  if (err) {return console.log(err);}
 	  console.log("saved to database");
 	  postFeed(283148272070769, date)
-	  let t = "saved " + Object.keys(query)[0] + " to database"
+	  let t = "Event " + Object.keys(query)[0] + " created, posted and saved to database"
 	  sendTextMessage(sender, t)
 	  })
 }
 
 //function to get info on all registered to the event
 function showRegistered(collection, sender) {
+	console.log("SHOW REGISTERED STARTED")
 	let cursor = collection.find();
 	if (!cursor) {sendTextMessage(sender, "No events found"); return false;}
 	cursor.toArray(function(err, result) {
