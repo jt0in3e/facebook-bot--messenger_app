ca1b1b93b775bcff1a30c2712031865c652d748b
Yura <yyinprogramming@gmail.com>
2016-11-01 14:00:50 +0200

    today in db

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function showRegistered(collection, sender, date) {
 	cursor.toArray(function(err, result) {
 		if (err) {return sendTextMessage(sender, "Err " +err)}
 		console.log("Here results for db: "+result[0]);
-		sendTextMessage(sender, result[0]);
+		sendTextMessage(sender, result[0][today]);
 	})
 }
 
