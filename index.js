3da578cf719f5a2f5b92cf36883b4e1382f22b73
Yura <yyinprogramming@gmail.com>
2016-11-01 12:34:35 +0200

    playing with today date

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function createEvent(collection, date, sender) {
 function showRegistered(collection, sender, date) {
 	console.log("SHOW REGISTERED STARTED");
 	let DATE = new Date();
-	let today = DATE.getDate()+ "/" + parseInt(DATE.getMonth())+ 1 + "/" + DATE.getFullYear();
+	let today = ("0"+DATE.getDate()).slice(-2)+ "/" + ("0"+(DATE.getMonth()+ 1)).slice(-2) + "/" + DATE.getFullYear();
 	console.log("TODAY is: " + today)
 	let cursor = collection.find({});
 	if (!cursor) {sendTextMessage(sender, "No events found"); return false;}
