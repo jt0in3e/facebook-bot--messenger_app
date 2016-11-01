c46d19d5216f09cdacc39be4f0c9ae83b3d1e3fa
Yura <yyinprogramming@gmail.com>
2016-11-01 14:12:48 +0200

    iterate over all dates

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ function showRegistered(collection, sender, date) {
 		if (err) {return sendTextMessage(sender, "Err " +err)}
 		console.log("Here results for db: "+result[0]);
 		for (let nb=0; nb<result.length; nb++) {
-		      sendTextMessage(sender, result[nb][today] + "\n");
+		      let keys = Object.keys(result[nb])[0];
+		      sendTextMessage(sender, keys + "\n");
 		}
 		
 	})
