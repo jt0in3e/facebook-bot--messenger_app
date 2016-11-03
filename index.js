61277cb1cd27534d4a724e6ee4e88cbb907050d8
Yura <yyinprogramming@gmail.com>
2016-11-03 17:11:58 +0200

    .count() seems to be assynchronus, needs callback

--- index.js
+++ index.js
@@ -90,7 +90,9 @@ function createEvent(collection, date, sender) {
 	  let queryTest = {};
 	  queryTest[date] = {$exists: true};
 	  console.log("queryTest: "+queryTest[date])
-	  let ex = collection.find(queryTest).count(); //trying this to find check solution http://stackoverflow.com/questions/8389811/how-to-query-mongodb-to-test-if-an-item-exists
+	  let ex = collection.find(queryTest).count(function(err, count) {
+	  	return count;
+	  }); //trying this to find check solution http://stackoverflow.com/questions/8389811/how-to-query-mongodb-to-test-if-an-item-exists
 	  if (ex) { 
 	  	console.log("COUNT: " + ex);
 	  	sendTextMessage(sender, "Event already exists"); 
