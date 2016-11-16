458728e9c6bed5245b873b2cfdeaf68803a8f7e7
Yura <yyinprogramming@gmail.com>
2016-11-16 22:37:38 +0200

    working on getting data from post/status/comments and add user/event v19

--- index.js
+++ index.js
@@ -416,7 +416,8 @@ MongoClient.connect(mongodbLink, function(err, database) {
 						let message = value["message"];
 						let count = docs[0][today]["registered"];
 						let persons = docs[0][today]["personsRegistered"];
-						//let replacement = {today: {"registered": 0, "personsRegistered": []}};
+						let replacement = {today: {"registered": 0, "personsRegistered": []}};
+						console.log("In replacement: [today]" + replacement[today])
 						if (!persons.length) {
 							//replacement[today]["registered"] = 1;
 							//replacement[today]["personsRegistered"] = [userData];
